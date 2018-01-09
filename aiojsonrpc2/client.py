5e7d662883c3ba433119923b2f77b1afe1a56314
Mathieu Lecarme <mlecarme@bearstech.com>
2018-01-09 10:35:20 +0100

    A client and a server

---
+++ aiojsonrpc2/client.py
@@ -0,0 +1,64 @@
+import logging
+import asyncio
+
+from aiohttp import web
+
+
+class Method:
+    def __init__(self, client, method):
+        self.client = client
+        self.method = method
+
+    async def __call__(self, **kwargs):
+        _id = self.client.id()
+        req = dict(jsonrpc="2.0", id=_id, method=self.method,
+                   params=kwargs)
+        self.client.responses[_id] = asyncio.Future()
+        await self.client.ws.send_json(req)
+        resp = await self.client.responses[_id]
+        return resp
+
+
+class Client:
+    def __init__(self, ws: web.WebSocketResponse):
+        self.ws = ws
+        self._id = 0
+        self.responses = dict()
+        self.listen = True
+        asyncio.ensure_future(self.listen_responses())
+
+    async def listen_responses(self):
+        while self.listen:
+            try:
+                resp = await self.ws.receive_json()
+            except TypeError as e:
+                continue
+            except RuntimeError as e:
+                if e.args[0] == 'WebSocket connection is closed.':
+                    return
+                else:
+                    raise e
+            else:
+                logging.debug(resp)
+                assert resp['jsonrpc'] == "2.0"
+                if 'id' in resp:
+                    self.responses[resp['id']].set_result(resp['result'])
+
+    def close(self):
+        self.listen = False
+
+    def id(self):
+        self._id += 1
+        return self._id
+
+    def proxy(self):
+        return Proxy(self)
+
+
+class Proxy:
+    def __init__(self, client: Client):
+        self.client = client
+
+    def __getattr__(self, method) -> Method:
+        return Method(self.client, method)
+
--- tests/test_web.py
+++ tests/test_web.py
@@ -1,15 +1,16 @@
 import logging
 from aiohttp import web
 
-from aiojsonrpc2 import Session
+from aiojsonrpc2.server import Session
+from aiojsonrpc2.client import Client
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 async def plop(request, ws, req):
     logging.debug(req.params)
-    ws.send_json(dict(jsonrpc="2.0", id=req._id,
-                      result="Hello {}".format(req.params['hello'])))
+    await ws.send_json(dict(jsonrpc="2.0", id=req._id,
+                            result="Hello {}".format(req.params['hello'])))
 
 
 async def jsonrpc_handler(request):
@@ -31,8 +32,10 @@ async def test_jsonrpc(test_client, loop):
     client = await test_client(app)
 
     j = await client.ws_connect('/')
-    await j.send_json(dict(jsonrpc="2.0", method="plop", id=0,
-                           params=dict(hello='world')))
-    r = await j.receive_json()
-    logging.debug(r)
+    c = Client(j)
+    proxy = c.proxy()
+    r = await proxy.plop(hello='world')
+    logging.debug('response %s', r)
     print(r)
+    assert r == "Hello world"
+    c.close()
