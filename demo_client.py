17a28b50bb9e216af449c9a84857a1f54bc60460
Mathieu Lecarme <mlecarme@bearstech.com>
2018-11-30 17:41:59 +0100

    dynamic demo

--- demo_client.py
+++ demo_client.py
@@ -1,15 +1,18 @@
 import asyncio
+import sys
+
 import aiohttp
 
 from aiojsonrpc2.client import Client
 
 
 async def main():
+    assert len(sys.argv) > 1, "I need one argument: a name."
     async with aiohttp.ClientSession() as session:
         async with session.ws_connect('http://localhost:8080') as ws:
             c = Client(ws)
             proxy = c.proxy()
-            r = await proxy.hello('world')
+            r = await proxy.hello(sys.argv[1])
             print(r)
             c.close()
 
