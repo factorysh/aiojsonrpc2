5b059862b2648b81b62ba3f54586738995e222c5
Mathieu Lecarme <mlecarme@bearstech.com>
2018-12-26 18:23:03 +0100

    More UNIX

--- aiojsonrpc2/transport/unix_socket.py
+++ aiojsonrpc2/transport/unix_socket.py
@@ -2,6 +2,8 @@ import struct
 import asyncio
 import json
 
+from aiojsonrpc2.session import Session
+
 
 class PascalStringTransport:
 
@@ -22,3 +24,28 @@ class PascalStringTransport:
         if isinstance(blob, bytes):
             blob = blob.decode('utf8')
         return json.loads(blob)
+
+
+def on_unix_client(**handlers):
+    async def on_client(reader: asyncio.StreamReader,
+                        writer: asyncio.StreamWriter):
+        transport = PascalStringTransport(reader, writer)
+        session = Session(transport)
+        for method, func in handlers.items():
+            session[method] = func
+        await session.run()
+    return on_client
+
+
+async def UnixServer(path, loop=None, **handlers):
+    server = await asyncio.start_unix_server(on_unix_client(**handlers),
+                                             path=str(path),
+                                             loop=loop)
+    print("server started:", path)
+    return server
+
+
+async def UnixClient(path, loop=None) -> Client:
+    return Client(PascalStringTransport(
+        *(await asyncio.open_unix_connection(str(path), loop=loop)))
+                  )
