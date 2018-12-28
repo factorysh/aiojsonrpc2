337784ff432b4a18116e2560978d65d10bdd38b4
Mathieu Lecarme <mlecarme@bearstech.com>
2018-12-28 19:36:37 +0100

    Universal iterator

--- aiojsonrpc2/transport/__init__.py
+++ aiojsonrpc2/transport/__init__.py
@@ -15,3 +15,36 @@ class AbstractTransport:
 
     def close(self):
         self.writer.close()
+
+
+class Iterator:
+    def __init__(self, transport):
+        self.queue = asyncio.Queue()
+        self.transport = transport
+        self.length = 0
+
+    def __aiter__(self):
+        return self
+
+    def __len__(self):
+        return self.length
+
+    async def __anext__(self):
+        if len(self) == 0:
+            try:
+                stanzas = await self.transport.receive_json()
+            except RuntimeError: # transport is closed
+                raise StopAsyncIteration
+            if not isinstance(stanzas, list):
+                stanzas = [stanzas]
+            self.length = len(stanzas)
+            for stanza in stanzas:
+                assert isinstance(stanza, dict), "stanza must be dict, not %s" % type(stanza)
+                assert stanza['jsonrpc'] == "2.0", "Only jsonrpc 2.0 is handled"
+                await self.queue.put(stanza)
+        try:
+            return await self.queue.get()
+        except RuntimeError as e:
+            if str(e) != "Event loop is closed":
+                raise e
+            # else the loop is closed, Queue yell for nothing
