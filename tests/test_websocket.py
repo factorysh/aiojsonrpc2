0b978f06c2d9efe9b2d0ddd6de5bf67725ddbc43
Mathieu Lecarme <mlecarme@bearstech.com>
2018-12-29 11:04:58 +0100

    More with

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -6,14 +6,9 @@ from aiohttp import web
 from aiojsonrpc2.session import Session
 from aiojsonrpc2.client import Client
 from aiojsonrpc2.transport.websocket import handler_factory
+from tests.utils import hello
 
 logging.basicConfig(level=logging.DEBUG)
-
-
-async def hello(name, __context=None):
-    print("ctx", __context)
-    asyncio.sleep(2)
-    return "Hello {}".format(name)
 
 
 async def test_jsonrpc(aiohttp_client, loop):
@@ -21,10 +16,9 @@ async def test_jsonrpc(aiohttp_client, loop):
     app.router.add_get('/', handler_factory(hello=hello))
 
     client = await aiohttp_client(app)
-    ws = await client.ws_connect('/')
-
-    c = Client(ws)
-    r = await c.stub.hello('world')
-    logging.debug('response %s', r)
-    assert r == "Hello world"
-    c.close()
+    async with client.ws_connect('/') as ws:
+        c = Client(ws)
+        r = await c.stub.hello('world')
+        logging.debug('response %s', r)
+        assert r == "Hello world"
+        await c.close()
