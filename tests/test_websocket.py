bbc63875eb730b1b6095f5b5ae9088dfbf983e77
Mathieu Lecarme <mlecarme@bearstech.com>
2018-12-29 20:32:41 +0100

    Better name. Multiple.

--- tests/test_websocket.py
+++ tests/test_websocket.py
@@ -11,7 +11,7 @@ from tests.utils import hello
 logging.basicConfig(level=logging.DEBUG)
 
 
-async def test_jsonrpc(aiohttp_client, loop):
+async def _test_websocket(aiohttp_client, loop):
     app = web.Application()
     app.router.add_get('/', handler_factory(hello=hello))
 
@@ -21,4 +21,21 @@ async def test_jsonrpc(aiohttp_client, loop):
         r = await c.stub.hello('world')
         logging.debug('response %s', r)
         assert r == "Hello world"
+        await c.close()
+
+
+async def test_websocket_multiple(aiohttp_client, loop):
+    app = web.Application()
+    app.router.add_get('/', handler_factory(hello=hello))
+
+    client = await aiohttp_client(app)
+    async with client.ws_connect('/') as ws:
+        c = Client(ws)
+        r1 = asyncio.ensure_future(c.stub.hello('Alice'))
+        r1.add_done_callback(print)
+        r2 = asyncio.ensure_future(c.stub.hello('Bob'))
+        r2.add_done_callback(print)
+        await asyncio.gather(r1, r2)
+        assert await r1 == "Hello Alice"
+        assert await r2 == "Hello Bob"
         await c.close()
