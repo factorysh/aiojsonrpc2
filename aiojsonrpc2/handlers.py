afbd9c54c7fa1a8c3533e5703a7fda58f15db079
Mathieu Lecarme <mlecarme@bearstech.com>
2018-12-30 23:54:56 +0100

    More docstring

--- aiojsonrpc2/handlers.py
+++ aiojsonrpc2/handlers.py
@@ -13,11 +13,19 @@ def _get_callable(obj, iter_, prefix):
 
 
 def handlers(*args, **kwargs):
+    """
+    Build a dict of key/coroutine function from stuffs.
+
+    args can be function, module or class.
+    * function: function has a name.
+    * module: all public functions (described in __all__ list), not starting with a _
+    * class: all methods with name not starting with a _
+    """
     for arg in args:
         if isinstance(arg, types.FunctionType): # It's a function
             kwargs[arg.__name__] = arg
-        elif isinstance(arg, types.ModuleType):
-            if '__all__' not in dir(arg): # It's a module
+        elif isinstance(arg, types.ModuleType): # It's a module
+            if '__all__' not in dir(arg):
                 continue
             kwargs.update(dict(_get_callable(arg, arg.__all__, arg.__name__)))
         else: # It's a class
