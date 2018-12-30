af625ef3084b951fcff9839cd73af1858135428f
Mathieu Lecarme <mlecarme@bearstech.com>
2018-12-30 19:58:32 +0100

    Extract handlers from a class

--- aiojsonrpc2/handlers.py
+++ aiojsonrpc2/handlers.py
@@ -1,6 +1,15 @@
 import asyncio
 
 
+def _get_callable(obj, iter_, prefix):
+    for m in iter_:
+        if m.startswith('_'):
+            continue
+        c = getattr(obj, m)
+        if callable(c):
+            yield "%s.%s" %(prefix, m), c
+
+
 def handlers(*args, **kwargs):
     for arg in args:
         # FIXME use instanceof
@@ -10,12 +19,11 @@ def handlers(*args, **kwargs):
         elif t == "<class 'module'>":
             if '__all__' not in dir(arg):
                 continue
-            for m in arg.__all__:
-                if m.startswith('_'):
-                    continue
-                c = getattr(arg, m)
-                if callable(c):
-                    kwargs["%s.%s" %(arg.__name__, m)] = c
+            kwargs.update(dict(_get_callable(arg, arg.__all__, arg.__name__)))
+        else:
+            kwargs.update(dict(_get_callable(arg, dir(arg),
+                                             arg.__class__.__name__)))
+
     for method in kwargs.values():
         assert asyncio.iscoroutinefunction(method)
     return kwargs
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -6,9 +6,20 @@ async def beuha():
     pass
 
 
-def test_handlers():
-    h = handlers(utils, beuha)
+class A:
+    def __init__(self, name):
+        self.name = name
+    async def hello(self):
+        return "Hello %s" % self.name
+    async def bonjour(self):
+        return "Bonjour %s" % self.name
+
+
+async def test_handlers(loop):
+    h = handlers(utils, beuha, A("Alice"))
     assert 'tests.utils.hello' in h
     assert 'tests.utils.let_it_crash' in h
     assert 'beuha' in h
-    assert len(h) == 3
+    assert 'A.hello' in h
+    assert 'A.bonjour' in h
+    assert len(h) == 5
